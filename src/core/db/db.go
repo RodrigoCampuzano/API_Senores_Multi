79cb2f3a1bc46b6c2146ed9eda3669b7567958fb
Rodrigo Campuzano <camcluod@gmail.com>
2025-03-13 15:40:12 -0600

    update (error db)

--- src/core/db/db.go
+++ src/core/db/db.go
@@ -11,7 +11,7 @@ import (
 func InitDB() (*sql.DB, error) {
     connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&tls=false",
         os.Getenv("DB_USER"), 
-        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_PASS"),
         os.Getenv("DB_HOST"), 
         os.Getenv("DB_PORT"), 
         os.Getenv("DB_NAME"))
